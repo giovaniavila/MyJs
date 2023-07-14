1243c135c1159049e66aed9ac3c756a4ea46b62f
Giovani <giovanicarvalhoavila@hotmail.com>
2023-07-14 11:55:14 -0300

    js-intermediario constructor_1

---
+++ js-intermediario/constructor/constructor.js
@@ -0,0 +1,7 @@
+function Carro(marcaAtribuida, precoAtribuido){
+    this.marca = marcaAtribuida;
+    this.preco = precoAtribuido; 
+}
+
+const Honda = new Carro('Honda', 14000);
+const Fiat = new Carro('Fiat uno', 15000);
---
+++ js-intermediario/constructor/notesConstructor.md
@@ -0,0 +1,5 @@
+# About this
+<p> the word "this" when you're coding refers the object, <strong> not the parameter </strong> </p>
+
+
+
