ce57f1d04839315cf26bbf6cdbd5217abb668fd5
giovani <giovanicarvalhoavila@hotmail.com>
2023-08-01 19:43:53 -0300

    type of objects, native, host and user

---
+++ js-intermediario/native_host_user/objects.js
@@ -0,0 +1,19 @@
+//objetos nativos são aqueles definidos na especificação da linguagem
+//servem para criar objetos
+// podem varias de ambiente para ambiente, por exemplo, no NodeJS não há o objeto Nodelist, mas existe nos browsers
+
+
+//exemplos de objetos nativos
+String
+Object
+Array
+Function
+
+
+//exemplo de um objeto de usuário, também podem ser de bibliotecas externas
+const pessoa = {
+    nome: 'Giovani' 
+}
+
+
+//Jquery é uma biblioteca javascript criada para resolver a insconsistência entre os browsers
